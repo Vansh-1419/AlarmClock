c0100d836c04037aa1a620d24b3c599decba3ef6
Vansh Tandon <Vanshtandon1423@gmail.com>
2022-02-26 17:27:33 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 const hours = document.querySelector(".hours");
 const minutes = document.querySelector(".minutes");
 const sec = document.querySelector(".sec");
@@ -5,37 +6,33 @@ const start = document.querySelector(".start");
 const sethours = document.querySelector("#h");
 const setminutes = document.querySelector("#m");
 const setseconds = document.querySelector("#s");
-sethours.value = `0`;
-setminutes.value = `0`;
-setseconds.value = `0`;
-sec.textContent = `0` + `${setseconds.value}`;
-minutes.textContent = `0` + `${setminutes.value}`;
-hours.textContent = `0` + `${sethours.value}`;
-sethours.addEventListener("click", () => {
-  sethours.value = ``;
-});
-setminutes.addEventListener("click", () => {
-  setminutes.value = ``;
-});
-setseconds.addEventListener("click", () => {
-  setseconds.value = ``;
-});
-const setstart = () => {
-  if (setseconds.value < 10) {
-    sec.textContent = `0` + `${setseconds.value}`;
-  } else if (setseconds.value > 10 && setseconds.value <= 60) {
-    sec.textContent = setseconds.value;
+const zone = document.querySelector(".zone");
+
+//Clock
+const clock = () => {
+  const current = new Date();
+  hours.textContent = current.getHours();
+  minutes.textContent = current.getMinutes();
+  sec.textContent = current.getSeconds();
+  zone.textContent = hours.textContent >= 12 ? `PM` : `AM`;
+  minutes.textContent = checkTime(minutes.textContent);
+  sec.textContent = checkTime(sec.textContent);
+  if (current.getHours() == 0) {
+    hours.textContent = 12;
   }
-  if (sethours.value < 10) {
-    hours.textContent = `0` + `${sethours.value}`;
-  } else if (sethours.value > 10 && sethours.value <= 60) {
-    hours.textContent = sethours.value;
+  if (current.getHours() > 12) {
+    hours.textContent = current.getHours() - 12;
   }
-  if (setminutes.value < 10) {
-    minutes.textContent = `0` + `${setminutes.value}`;
-  } else if (setminutes.value > 10 && setminutes.value <= 60) {
-    minutes.textContent = setminutes.value;
+  setTimeout(clock, 1000);
+};
+const checkTime = (i) => {
+  if (i < 10) {
+    i = "0" + i;
+    return i;
+  } else {
+    return i;
   }
 };
 
-const st = () => {};
+//Setting the alarm
+const settingAlarm = () => {};
---
+++ index2.js
@@ -0,0 +1,39 @@
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const sec = document.querySelector(".sec");
+const start = document.querySelector(".start");
+const sethours = document.querySelector("#h");
+const setminutes = document.querySelector("#m");
+const setseconds = document.querySelector("#s");
+sethours.value = `0`;
+setminutes.value = `0`;
+setseconds.value = `0`;
+sec.textContent = `0` + `${setseconds.value}`;
+minutes.textContent = `0` + `${setminutes.value}`;
+hours.textContent = `0` + `${sethours.value}`;
+sethours.addEventListener("click", () => {
+  sethours.value = ``;
+});
+setminutes.addEventListener("click", () => {
+  setminutes.value = ``;
+});
+setseconds.addEventListener("click", () => {
+  setseconds.value = ``;
+});
+const setstart = () => {
+  if (setseconds.value < 10) {
+    sec.textContent = `0` + `${setseconds.value}`;
+  } else if (setseconds.value > 10 && setseconds.value <= 12) {
+    sec.textContent = setseconds.value;
+  }
+  if (sethours.value < 10) {
+    hours.textContent = `0` + `${sethours.value}`;
+  } else if (sethours.value > 10 && sethours.value <= 12) {
+    hours.textContent = sethours.value;
+  }
+  if (setminutes.value < 10) {
+    minutes.textContent = `0` + `${setminutes.value}`;
+  } else if (setminutes.value > 10 && setminutes.value <= 12) {
+    minutes.textContent = setminutes.value;
+  }
+};
